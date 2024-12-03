26549a98022540453d1abcd0a4c4e1fcc0eae63f
Sindy Araujo <aellen851@gmail.com>
2024-12-03 11:25:00 -0300

    Add files via upload

---
+++ Aula 02 - Python/Aula 2.py
@@ -0,0 +1,8 @@
+print('Bom dia, Carlos!')
+
+nome_usuario = 'Sindy Araujo' # tipo -> str = string
+idade_usuario = 20 # tipo -> int = integer
+altura_usuario = 1.63 # tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print('Seja Bem-vindo',nome_usuario,',Sua idade é',idade_usuario,'anos ,Você tem', altura_usuario,'metros. Você é maior de idade?',maior_idade)
